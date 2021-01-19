3f98ba0231350d992d9f4985d97f9ace1406ced5
xsebastianx <sarnavat1@gmail.com>
2021-01-19 18:33:03 -0500

    Created navbar with links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'></Route>
+          <Route exact path='/projects'></Route>
+        </Switch>
+      </Router>
 
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
+
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            <ul className='InternalLinks'>
+                <li>
+                    <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+                </li>
+                <li>
+                    <Link to='/projects' style={{ textDecoration: 'none', color: 'white' }}>Projects</Link>
+                </li>
+            </ul>
+            <ul className='ExternalLinks'>
+                <li>
+                    <a href='https://github.com/xsebastianx' target="_blank">
+                        <FontAwesomeIcon icon={faGithub} color='white' size='2x' />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.linkedin.com/in/sebastianarnavat/' target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} color='white' size='2x' />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
