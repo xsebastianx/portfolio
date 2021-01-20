38b51b81623044ad7c8361e40882c6962d00e2bc
xsebastianx <sarnavat1@gmail.com>
2021-01-19 19:55:11 -0500

    Added profile image

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Home.css'
+import propic from '../../assests/propic.jpg';
 
 const Home = () => {
     return (
-        <div>
+        <div className='Home'>
             <div className='Header'>
+                <img src={propic} className='ProPic'></img>
+                <div className='SkillsBar'>
 
+                </div>
             </div>
+            <div className='About'>
+                <h1>About</h1>
+            </div>
+
         </div>
     )
 }
